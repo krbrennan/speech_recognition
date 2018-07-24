dbcab712acc741f4a4916b776ed24c34b47df9c8
krbrennan <krb.brennan@gmail.com>
2018-07-24 16:33:16 -0400

    Working. Doesn't record initial words but inserts a div

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 recognition.start();
 
+let p = document.createElement("p");
+const words = document.querySelector('.words');
+
 
 // words.appendChild(p);
 
 
 recognition.addEventListener('result', e => {
-  console.log(e)
+  // console.log(e)
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join(' ')
+  p.textContent = transcript + '.  ';
+  if(e.results[0].isFinal){
+    p = document.createElement('p');
+    words.appendChild(p)
+  }
 })
 
 recognition.addEventListener('end', recognition.start);
 //
-recognition.onresult = function(e) {
-  let p = document.createElement("p");
-  const words = document.querySelector('.words');
-  // console.log(e.results[0].isFinal)
-  if(e.results[0].isFinal == true){
-    p = e.results[0][0].transcript + ".  "
-    words.append(p)
-    // recognition.start;
-  }
-}
+// recognition.onresult = function(e) {
+//   let p = document.createElement("p");
+//   const words = document.querySelector('.words');
+//   if(e.results[0].isFinal == true){
+//     p = e.results[0][0].transcript + ".  "
+//     p = p[0].toUpperCase() + p.slice(1,-1)
+//     words.append(p)
+//   }
+// }
