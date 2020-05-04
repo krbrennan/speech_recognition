6902488df62528054d14a59c69ed5fbf7ebb4dca
krbrennan <krb.brennan@gmail.com>
2020-05-04 00:32:29 -0400

    working as desired with start and stop buttons. Next, style the buttons a little better

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 // Considers different browsers' implementations of speech recognition
+// Since this app only works on Chrome, it's mostly here as a reminder to implement
+// support for different browsers whenever they decide to integrate useable voice recognition tools
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-let recognition = new SpeechRecognition();
+let recognition;
 
 const startRecording = document.getElementById('record-btn');
 const endRecording = document.getElementById('record-end');
 
-let numberOfP = 0
-
-let stopRecording = false;
-
-  // creates a paragraph HTML element to transcribe dictations 
-  // const p = document.createElement("p");
-  // let words = document.querySelector('.words');
-
-
-
+// 
+// 
 startRecording.onclick = function(){
   beginRecording();
 }
+// 
+// 
+function completedSentence(transcript){
+    const p = document.createElement("p");
+    const words = document.querySelector('.words');
+  
+     // formatting to include basic punctuation
+     capitalizedTranscript = transcript.charAt(0).toUpperCase() + transcript.substr(1)
+     p.textContent = capitalizedTranscript + '.  ';
+
+    words.append(p)
+}
+// 
+// 
 
 function beginRecording(){
+  // console.log('ANNOTATING')
 
-console.log(recognition)
-  console.log('ANNOTATING')
   // initializes speech recognition variable and begins listening to mic
-  // const recognition = new SpeechRecognition();
+  // I used to have "new SpeechRecognition" at the top of the script, globally, 
+  // but when I'd stop recording then begin again I would get duplicate text
+  // and I couldn't figure out why the script seemed to be running multiple instances.
+  // I've reasoned that by creating the object here, I'm able to ensure that only one
+  // instance is created when beginRecording() is called.
+  recognition = new SpeechRecognition();
   recognition.interimResults = true;
   recognition.start();
   
@@ -36,97 +48,31 @@ console.log(recognition)
       .map(result => result.transcript)
       .join('')
 
-
-      function completedSentence(){
-
-
-        // console.log(p.textContent)
-        const p = document.createElement("p");
-        let words = document.querySelector('.words');
-
-         // formatting to include basic punctuation
-         capitalizedTranscript = transcript.charAt(0).toUpperCase() + transcript.substr(1)
-         p.textContent = capitalizedTranscript + '.  ';
-        // p = document.createElement('p');
-        // words.appendChild(p)
-        words.append(p)
-      }
-    
-    
-    
     if(e.results[0].isFinal){
-      // p = document.createElement('p');
-      // words.appendChild(p)
-      setTimeout(completedSentence(), 1000)
+      num = 0
+      setTimeout(completedSentence(transcript), 1000)
     }
   });
-  
   recognition.addEventListener('end', recognition.start)
-
 }
+// 
+// 
 
-
-  
-
-  
-
-
-
-// endRecording.addEventListener('click', () => {
-//   console.log('STOPPPPP')
-//   recognition.stop();
-// });
-
+// 
+// 
 endRecording.onclick = function(){
   endDictation();
 }
+// 
+// 
 
+// 
+// 
 function endDictation(){
-  recognition.removeEventListener('end', recognition.start )
-  recognition.stop;
-  recognition.abort;
-  console.log('successfully aborted recording')
+  recognition.removeEventListener('end', recognition.start)
+  recognition.stop();
+  recognition.abort();
+  num = 0;
+  console.log('successfully aborted recording');
+  return;
 }
-
-
-
-// function annotate(){
-
-
-//   console.log('ANNOTATING')
-// // initializes speech recognition variable and begins listening to mic
-// // const recognition = new SpeechRecognition();
-// recognition.interimResults = true;
-// recognition.start();
-
-// // creates a paragraph HTML element to transcribe dictations 
-// let p = document.createElement("p");
-// const words = document.querySelector('.words');
-// words.appendChild(p);
-
-
-// recognition.addEventListener('result', e => {
-//   const transcript = Array.from(e.results)
-//     .map(result => result[0])
-//     .map(result => result.transcript)
-//     .join('')
-  
-// // formatting to include basic punctuation
-//   capitalizedTranscript = transcript.charAt(0).toUpperCase() + transcript.substr(1)
-//   p.textContent = capitalizedTranscript + '.  ';
-//   if(e.results[0].isFinal){
-//     // p = document.createElement('p');
-//     // words.appendChild(p)
-//     setTimeout(completedSentence, 1000)
-//   }
-// })
-
-// function completedSentence(){
-//   p = document.createElement('p');
-//   words.appendChild(p)
-// }
-
-// // recognition.addEventListener('stop', recognition.stop);
-// recognition.addEventListener('end', recognition.start);
-
-// }
